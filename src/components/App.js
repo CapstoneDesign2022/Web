4225d3da15d18c5f9abccbe37cbb2cb1a4bb58f9
skyla2692 <reloxo99@gmail.com>
2022-04-20 14:25:11 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Chart from './Chart';
-import Navigation from './Navigation';
 
 function App() {
   return (
     <div>
-      {/* <Navigation/> */}
       <Chart/>
     </div>
   );
